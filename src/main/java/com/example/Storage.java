582f0d19e4590068dcaee16515b92d19efd58871
Szász Benedek <benedekszasz7@gmail.com>
2026-03-23 13:41:50 +0100

    .gitignore hozzáadása

--- src/main/java/com/example/Storage.java
+++ src/main/java/com/example/Storage.java
@@ -2,19 +2,24 @@ package com.example;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Storage {
-    public void readFile() {
+    public static List<Vehicle> readFile() {
         try {
-            tryReadFile();
+            return tryReadFile();
         } catch (FileNotFoundException e) {
             System.err.println(e.getMessage());
+            return null;
         }
     }
-    private void tryReadFile() throws FileNotFoundException {
+    private static List<Vehicle> tryReadFile() throws FileNotFoundException {
+        List<Vehicle> vehicleList = new ArrayList<>();
         File file = new File("jaribt.txt");
-        
         Scanner sc = new Scanner(file);
+
+        return vehicleList;
     }
 }
